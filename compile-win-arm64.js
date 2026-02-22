eec3ee37e327be75689138d50022d36d820dc63e
dapize <danielpz@outlook.com>
2026-02-22 18:19:10 +0100

    WIP: chore: configure mail.yml for github actions and compile-win-arm64.js for accept alias

--- compile-win-arm64.js
+++ compile-win-arm64.js
@@ -1,30 +1,32 @@
-import { existsSync, readFileSync, mkdirSync, copyFileSync } from "node:fs";
-import { unlink } from "node:fs/promises";
 import { execSync } from "node:child_process";
+import { copyFileSync, existsSync, mkdirSync, readFileSync } from "node:fs";
 import process from "node:process";
+
 import { inject } from "postject";
 
+import { unlink } from "node:fs/promises";
+
 (async () => {
-  const exePath = "./bin/mediafiredl_win-arm64.exe";
-  const seaBlobPath = "./sea-prep.blob";
-  try {
-    for await (const filePath of [exePath, seaBlobPath]) {
-      if (existsSync(filePath)) {
-        await unlink(filePath);
-      }
-    }
-    execSync("node --no-warnings --experimental-sea-config sea-config.json");
-    if (!existsSync("./bin")) {
-      mkdirSync("./bin");
-    }
-    copyFileSync(process.execPath, exePath);
-    execSync(`signtool remove /s ${exePath}`);
-    const resourceBlob = readFileSync(seaBlobPath);
-    await inject(exePath, "NODE_SEA_BLOB", resourceBlob, {
-      sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
-    });
-    await unlink(seaBlobPath);
-  } catch (err) {
-    throw new Error(err);
-  }
+	const exePath = "./bin/mediafiredl_win-arm64.exe";
+	const seaBlobPath = "./sea-prep.blob";
+	try {
+		for await (const filePath of [exePath, seaBlobPath]) {
+			if (existsSync(filePath)) {
+				await unlink(filePath);
+			}
+		}
+		execSync("node --no-warnings --experimental-sea-config sea-config.json");
+		if (!existsSync("./bin")) {
+			mkdirSync("./bin");
+		}
+		copyFileSync(process.execPath, exePath);
+		execSync(`signtool remove /s ${exePath}`);
+		const resourceBlob = readFileSync(seaBlobPath);
+		await inject(exePath, "NODE_SEA_BLOB", resourceBlob, {
+			sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
+		});
+		await unlink(seaBlobPath);
+	} catch (err) {
+		throw new Error(err);
+	}
 })();
